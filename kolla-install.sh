45b7eafedb31eb1448297413047e3f372fa0b539
Jake Hutchinson <jake@stackhpc.com>
2025-04-15 14:59:42 +0100

    Deploy containers

--- kolla-install.sh
+++ kolla-install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo yum -y install wget python-devel libffi-devel gcc openssl-devel libselinux-python python-virtualenv
+sudo dnf -y install wget python-devel libffi-devel gcc openssl-devel dbus-devel dbus-glib-devel
 
 if [[ ! -f ~/.ssh/id_rsa ]]; then
   ssh-keygen  -f ~/.ssh/id_rsa -t rsa -N ''
@@ -10,20 +10,15 @@ fi
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys 
 
 if [[ ! -d ./kolla-ansible ]]; then
-  git clone https://github.com/openstack/kolla-ansible
-  pushd kolla-ansible
-  git fetch https://git.openstack.org/openstack/kolla-ansible refs/changes/03/633503/1 && git cherry-pick FETCH_HEAD
-  popd
+  git clone https://github.com/openstack/kolla-ansible -b stable/2024.1
 fi
-virtualenv kolla-venv
-if  [[ ! -L kolla-venv/lib/python2.7/site-packages/selinux ]]; then
-  ln -s /usr/lib64/python2.7/site-packages/selinux/ kolla-venv/lib/python2.7/site-packages/
-fi
+python3 -m venv kolla-venv
 source kolla-venv/bin/activate
 pip install -U pip
 pip install -U setuptools
 pip install ./kolla-ansible
-pip install ansible
+ansible-galaxy install -r ./kolla-ansible/requirements.yml
+pip install ansible docker dbus-python
 
 sudo mkdir -p /etc/kolla
 sudo chown $USER: -R /etc/kolla/
@@ -38,6 +33,7 @@ if [[ ! -e /etc/kolla/passwords.yml ]]; then
 fi
 ssh-keyscan 127.0.0.1 >> ~/.ssh/known_hosts
 kolla-ansible -i /etc/kolla/inventory/all-in-one bootstrap-servers
+sudo usermod -aG docker $USER
 
 if ! groups | grep docker >/dev/null; then
   echo "Please log out then log back in to pick up Docker group membership"
